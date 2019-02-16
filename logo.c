7272dc7a1305e9e74265efa83ba9890b29af0b48
Mateusz Dukat <d3s.prv@mail.ru>
2019-02-16 23:46:55 +0100

    logo.c: Patch 1
     - Fixed bool types
     - Changed speed to more legit one
     - Added loopCount variable
     - Constant variables are now constant (u dont say)
     - Some more debugging info

--- logo.c
+++ logo.c
@@ -1,25 +1,27 @@
 #include <stdio.h> // printf
 #include <unistd.h> // usleep
 #include <stdlib.h> // rand
+#include <stdbool.h> // bool, true, false
 
 // uncomment to debug
 //#define DEBUG
 
 #define Y_SIZE 20 // You can change these values as you want!
 #define X_SIZE 50
-#define SPEED 50
+#define SPEED 150
 
 
 char screenBuffer[Y_SIZE][X_SIZE];
-char logoChar = 'A';
-char borderChar = '#';
+const char logoChar = 'A';
+const char borderChar = '#';
+long long loopCount = 0;
 struct logo {
 	unsigned int x;
 	unsigned int y;
 	unsigned int last_x;
 	unsigned int last_y;
-	char move_right;	// bool
-	char move_down;		// bool
+	bool move_right;
+	bool move_down;
 } logoPos;
 
 int main(){
@@ -42,19 +44,19 @@ int main(){
 		screenBuffer[i][X_SIZE-1] = borderChar;
 	}
 
-	// setup "logo" position		// this can be done better
+	// setup "logo" position
 	logoPos.x = X_SIZE+1;
 	logoPos.y = Y_SIZE+1;
 	while(logoPos.x < 0 || logoPos.x > X_SIZE-1){
-		logoPos.x = 5; //should be rand()
+		logoPos.x = rand() % X_SIZE-1;
 	}
 	while(logoPos.y < 0 || logoPos.y > Y_SIZE-1){
-		logoPos.y = 5; //should be rand()
+		logoPos.y = rand() % Y_SIZE-1;
 	}
 	logoPos.last_x = 1; // here 1 is safe becouse (0, 0) is border
 	logoPos.last_y = 1;
-	logoPos.move_right = 1; // this actually should be bool but oh well
-	logoPos.move_down = 1;
+	logoPos.move_right = true;
+	logoPos.move_down = true;
 
 	// HERE BEGINS FUN (aka main loop)
 
@@ -71,7 +73,7 @@ int main(){
 
 		// some debugging
 #ifdef DEBUG
-		printf("X: %d, Y: %d, RAND: %d\n", logoPos.x, logoPos.y, rand());
+		printf("X: %d, Y: %d, RAND: %d, RIGHT: %d, DOWN: %d, LOOP: %lld\n", logoPos.x, logoPos.y, rand(), logoPos.move_right, logoPos.move_down, loopCount);
 #endif
 		// print screenBuffer
 		for(int i = 0; i<Y_SIZE; i++){
@@ -86,25 +88,25 @@ int main(){
 		logoPos.last_y = logoPos.y;
 		
 		if(logoPos.x == 1){
-			logoPos.move_right = 1;
+			logoPos.move_right = true;
 		}
 		if(logoPos.x == X_SIZE-2){
-			logoPos.move_right = 0;
+			logoPos.move_right = false;
 		}
 
 		if(logoPos.y == 1){
-			logoPos.move_down = 1;
+			logoPos.move_down = true;
 		}
 		if(logoPos.y == Y_SIZE-2){
-			logoPos.move_down = 0;
+			logoPos.move_down = false;
 		}
 
-		if(logoPos.move_right == 1){
+		if(logoPos.move_right == true){
 			logoPos.x += 1;
 		}else{
 			logoPos.x -= 1;
 		}
-		if(logoPos.move_down == 1){
+		if(logoPos.move_down == true){
 			logoPos.y += 1;
 		}else{
 			logoPos.y -= 1;
@@ -112,6 +114,7 @@ int main(){
 
 		// sleep (screen flickering fix)
 		usleep(SPEED*1000);
+		loopCount += 1;
 	}
 	return 1;
 }
